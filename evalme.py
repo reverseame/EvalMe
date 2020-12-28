2e8e743549939c3a3a46a23491311fe3685ac4e8
RazviOverflow <razvioverflow@gmail.com>
2020-12-28 05:55:43 -0500

    Implented parametrized SLICE_IN_SECONDS and default values are now being printed

--- evalme.py
+++ evalme.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 '''
+pip3 install -r requirements.txt
+
 @Authors:
 	Razvan Raducu
 	Ricardo J Rodriguez
 	Pedro Álvarez
 
-We used pipreqs to generate requirements.txt
-https://github.com/bndr/pipreqs
+@Limiattions:
+	Both hyperfine and Python's subprocess execute plain Bourne shell (/bin/sh) instead of Bash (/bin/bash). That is, commands like ./evalme.py 'for((i=0;i<1000;i++));do aux=$((i*i)); done' will not work because that's Bash sintax. In order to eval it, an equivalente bourne shell command must be provided ./evalme.py 'i=0; while [ $i -le 1000 ]; do i=$((i+1));aux=$((i*i)); done'
+
+@Additional info:
+	We used pipreqs to generate requirements.txt https://github.com/bndr/pipreqs
 '''
 import argparse
 import subprocess
@@ -15,23 +20,26 @@ import psutil
 import time
 import humanfriendly
 
+
 def usage():
 	usage = "Use EvalMe with the exact syntax as hyperfine. Example:\n"
 	usage += "\tevalme.py \"ls -R\"\n"
 	usage += "\tevalme.py --min-runs 5 \"grep -R TODO *\"\n"
-	usage += "\tevalme.py -r 150 \"ls -laihR\"\n\n"
+	usage += "\tevalme.py -r 150 \"ls -laihR\"\n"
+	usage += "\tevalme.py 'i=0; while [ $i -le 1000 ]; do i=$((i+1));aux=$((i*i)); done' -r 10000 -s 0.001\n\n"
 
 	return usage
 
 def parse_arguments():
 	parser = argparse.ArgumentParser(description="EvalMe, an open-source benchmarking tool.", usage=usage())
 	parser.add_argument("command", help="The command or binary to benchmark")
+	parser.add_argument("-s", "--slice", action="store", dest="SLICE_IN_SECONDS", type=float, help="(Default = 0.1) Defines the slice (in seconds) EvalMe will poll the spawned process and check its memory usage.")
 	parser.add_argument("-w", "--warmup", help="Perform NUM warmup runs before the actual benchmark. This can be used to fill (disk) caches for I/O-heavy programs.")
-	parser.add_argument("-m", "--min-runs", help="Perform at least NUM runs for each command (default: 10).", dest="min-runs")
-	parser.add_argument("-M", "--max-runs", help="Perform at most NUM runs for each command. By default, there is no limit.", dest="max-runs")
-	parser.add_argument("-r", "--runs", help="Perform exactly NUM runs for each command. If this option is not specified, hyperfine automatically determines the number of runs.")
-	parser.add_argument("-p", "--prepare <CMD>...", help="Execute CMD before each timing run. This is useful for clearing disk caches, for example. \nThe --prepare option can be specified once for all commands or multiple times, once for each command. In the latter case, each preparation command will be run prior to the corresponding benchmark command.")
-	parser.add_argument("-c", "--cleanup <CMD>", help="Execute CMD after the completion of all benchmarking runs for each individual command to be benchmarked. This is useful if the commands to be benchmarked produce artifacts that need to be cleaned up.")
+	#parser.add_argument("-m", "--min-runs", help="Perform at least NUM runs for each command (default: 10).", dest="min-runs")
+	#parser.add_argument("-M", "--max-runs", help="Perform at most NUM runs for each command. By default, there is no limit.", dest="max-runs")
+	parser.add_argument("-r", "--runs", action="store", type=int, help="(Default = 10) Perform exactly RUNS runs for each command.")
+	parser.add_argument("-p", "--prepare", help="Execute the command before each timing run. This is useful for clearing disk caches, for example. \nThe --prepare option can be specified once for all commands or multiple times, once for each command. In the latter case, each preparation command will be run prior to the corresponding benchmark command.")
+	parser.add_argument("-c", "--cleanup", help="Execute the command after the completion of all benchmarking runs for each individual command to be benchmarked. This is useful if the commands to be benchmarked produce artifacts that need to be cleaned up.")
 	arguments = parser.parse_args()
 
 	return arguments
@@ -42,8 +50,9 @@ def launch_hyperfine(arguments):
 	arguments_copy = copy.deepcopy(arguments)
 	command = arguments_copy.command
 
-	# Delete ([("command", binary)...])
+	# Delete all commands that are not compatible with hyperfine
 	del vars(arguments_copy)['command']
+	del vars(arguments_copy)['SLICE_IN_SECONDS']
 
 	arguments_array = []
 	for argument,value in vars(arguments_copy).items():
@@ -58,14 +67,23 @@ def launch_hyperfine(arguments):
 
 
 def check_ram_usage(arguments):
-	runs = 10
+
 	if arguments.runs is not None:
-		runs = int(arguments.runs)
+		RUNS = arguments.runs
+	else :
+		RUNS = 10
+
+	if arguments.SLICE_IN_SECONDS is not None:
+		SLICE_IN_SECONDS = arguments.SLICE_IN_SECONDS
+	else:
+		SLICE_IN_SECONDS = 0.1
+	
 
 	#print("RUNS -> " + str(runs))
-	SLICE_IN_SECONDS = 0.1
+	print("RUNS VALUE IS ->" + str(RUNS))
+	print("SLICE VALUE IS ->" + str(SLICE_IN_SECONDS))
 	resultTable = []
-	for i in range(0,runs):
+	for i in range(0,RUNS):
 		proc = subprocess.Popen(arguments.command, shell=True, stdout=subprocess.DEVNULL)
 		while proc.poll() is None:
 			p = psutil.Process(proc.pid)
@@ -89,8 +107,8 @@ def check_ram_usage(arguments):
 		counter += 1
 	virtual_memory = virtual_memory / counter
 
-	print("[+] Average of real memory used while executing \"" + str(arguments.command) + "\" " +str(runs) + " times: " + humanfriendly.format_size(real_memory))
-	print("[+] Average of virtual memory used while executing \"" + str(arguments.command) +"\" "+ str(runs) + " times: " + humanfriendly.format_size(virtual_memory))
+	print("[+] Average of real memory used while executing \"" + str(arguments.command) + "\" " +str(RUNS) + " times: " + humanfriendly.format_size(real_memory))
+	print("[+] Average of virtual memory used while executing \"" + str(arguments.command) +"\" "+ str(RUNS) + " times: " + humanfriendly.format_size(virtual_memory))
 	#print("ResultTable -> ")
 	#print(resultTable)
 	#for memory in resultTable:
